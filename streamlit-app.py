6eadf6a37b3a3fd464044ecad91ad92306f22a13
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2023-06-17 00:50:47 +0300

    Improve main page

--- streamlit-app.py
+++ streamlit-app.py
@@ -2,19 +2,34 @@ import streamlit as st
 import pandas as pd
 import pickle
 
-st.title("Hello world")
-
-# X_test = ["Good morning, you lovely people <3"]
-
-text = [st.text_area('Input text to predict:')]
 
 loaded_model = pickle.load(open('data/test-model.pkl', 'rb'))
 loaded_decoder = pickle.load(open('data/decoder.pkl', 'rb'))
 
-result = loaded_model.predict(text)
-st.write(loaded_decoder.classes_)
 
-# st.write(result)
-st.write(loaded_decoder.inverse_transform(result))
+def update_result(text):
 
-st.write(loaded_model.predict_log_proba(text))
+    result = loaded_model.predict([text])
+    # st.write(loaded_decoder.classes_)
+
+    # st.write(result)
+    st.subheader("Predicted cyberbullying class:")
+    st.header(loaded_decoder.inverse_transform(result)[0])
+
+    # st.write(loaded_model.predict_log_proba(text))
+
+
+def on_change_source():
+    st.write("huh")
+
+
+st.title("Detect and classify cyberbullying")
+
+# X_test = ["Good morning, you lovely people <3"]
+
+form = st.form(key='main_form')
+source = form.text_input(label="Enter text to predict cyberbullying:")
+submit_button = form.form_submit_button(label='Submit')
+
+if submit_button:
+    update_result(source)
